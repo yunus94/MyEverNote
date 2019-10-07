e046a8fe38224c9663cea7626d91a66aecea1bf5
yunus94 <yunusonal94@gmail.com>
2019-10-08 00:42:01 +0300

    Entity Framework and DatabaseContext added

---
+++ MyEvernote.DataAccessLayer/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using MyEvernote.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEvernote.DataAccessLayer
+{
+   public class DatabaseContext:DbContext
+    {
+        public DbSet<EvernoteUser> EvernoteUsers { get; set; } //evernoteUser ı kullanmak için referans olarak entities katmanı eklendi.
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Liked> Likes { get; set; }
+
+    }
+}
